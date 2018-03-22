898cbef322b5d3e48501227e1c4c043f8f3b0878
Andres Higueros <higueros71@hotmail.com>
2018-03-22 14:22:44 -0600

    agregando la fase preliminar del proyecto de conecta5

---
+++ Proyecto/Conecta5/Conecta5/Models/IConcecta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conecta5.Models
+{
+    interface IConcecta<T>
+    {
+        bool CheckWin();
+        void BuildBoard(T[,] board);
+        T[,] Move(T valor);
+    }
+}
